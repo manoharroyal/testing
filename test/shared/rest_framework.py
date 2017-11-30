6e9b94de2b86ecd1ac767f18095bf3eaad602df1
emanohar80 <emanohar80@gmail.com>
2017-11-30 12:37:35 +0530

    modified token generation functions

--- test/shared/rest_framework.py
+++ test/shared/rest_framework.py
@@ -12,8 +12,8 @@ with open(path + "/env/configuration.yaml", 'r') as stream:
     except yaml.YAMLError as exc:
         print "Cannot able to access input configuration"
 
+USER_LOGIN_URL = config_data['USER_LOGIN_URL']
 LOGIN_URL = config_data['LOGIN_URL']
-AGENT_LOGIN_URL = config_data['AGENT_URL']
 
 """ Helper functions are goes here """
 
@@ -42,43 +42,12 @@ class RestAPI(object):
         if utype == 'customer':
             self.id_token, self.customerId = self.generate_customer_token()
         if utype == 'invalid':
-            self.id_token, self.customerId = self.generate_invalid_token()
+            self.id_token, self.customerId = self.generate_customer_invalid_token()
         if utype == 'sysops':
             self.id_token, self.customerId = self.generate_sysops_token()
         if utype == 'agent':
             self.id_token, self.customerId = self.generate_agent_token()
         self.id_token = "Bearer " + self.id_token
-
-    def generate_sysops_token(self):
-        data = {
-            "client_id": config_data['client_id'],
-            "username": config_data['sysops_username'],
-            "password": config_data['sysops_password'],
-            "connection": config_data['connection'],
-            "grant_type": config_data['grant_type'],
-            "scope": config_data['scope']
-        }
-
-        header = {
-            "Content-Type": "application/json"
-        }
-        response = requests.post(LOGIN_URL, data=json.dumps(data),
-                                 headers=header)
-        try:
-            encode = str(response.json()['id_token'])
-        except:
-            print "Cannot able to parse the token in Login"
-            sys.exit(1)
-        return str(response.json()['id_token']), response.json()
-
-    def generate_agent_token(self):
-        """ Generation of agent_token """
-
-        agent_user = config_data['agent_username']
-        agent_password = config_data['agent_password']
-        data = {"username": agent_user, "password": agent_password}
-        response = requests.post(AGENT_LOGIN_URL, data=json.dumps(data))
-        return response.json()['token'], response.json()['customer_id']
 
     def generate_customer_token(self):
         """ Generation of token """
@@ -87,15 +56,15 @@ class RestAPI(object):
             "client_id": config_data['client_id'],
             "username": config_data['customer_username'],
             "password": config_data['customer_password'],
-            "connection": config_data['connection'],
-            "grant_type": config_data['grant_type'],
-            "scope": config_data['scope']
+            "connection": config_data['customer_connection'],
+            "grant_type": config_data['customer_grant_type'],
+            "scope": config_data['customer_scope']
         }
 
         header = {
             "Content-Type": "application/json"
         }
-        response = requests.post(LOGIN_URL, data=json.dumps(data),
+        response = requests.post(USER_LOGIN_URL, data=json.dumps(data),
                                  headers=header)
         try:
             encode = str(response.json()['id_token'])
@@ -104,16 +73,63 @@ class RestAPI(object):
             print "Cannot able to parse the token in Login"
             sys.exit(1)
         decode = jwt.decode(encode, 'secret', algorithm=['RS256'], verify=False)
-        customerId = (decode['customerId'])
-        return str(response.json()['id_token']), customerId
+        customer_id = decode['customerId']
+        return encode, customer_id
 
-    def generate_invalid_token(self):
+    def generate_customer_invalid_token(self):
         """ Function to generate invalid token """
-        token_url, customerId = self.generate_customer_token()
-        jwt_dict = jwt.decode(token_url, verify=False)
-        jwt_dict['customerId'] = customerId + '234'
-        token_url = jwt.encode(jwt_dict, 'secret', algorithm='HS256')
-        return token_url, customerId
+        encode, customer_id = self.generate_customer_token()
+        encode = encode + 'as'
+        return encode, customer_id
+
+    def generate_sysops_token(self):
+
+        data = {
+            "client_id": config_data['client_id'],
+            "client_secret": config_data['client_secret'],
+            "audience": config_data["audience"],
+            "grant_type": config_data['grant_type'],
+            "username": config_data['sysops_username'],
+            "password": config_data['sysops_password'],
+            "realm": config_data['sysops_realm'],
+            "scope": config_data['scope']
+        }
+
+        header = {
+            "Content-Type": "application/json"
+        }
+        response = requests.post(LOGIN_URL, data=json.dumps(data), headers=header)
+        try:
+            encode = str(response.json()['access_token'])
+        except:
+            print "Cannot able to parse the token in Login"
+            sys.exit(1)
+        return encode, response.json()
+
+    def generate_agent_token(self):
+        """ Generation of agent_token """
+
+        data = {
+            "client_id": config_data['client_id'],
+            "client_secret": config_data['client_secret'],
+            "audience": config_data["audience"],
+            "grant_type": config_data['grant_type'],
+            "username": config_data['agent_username'],
+            "password": config_data['agent_password'],
+            "realm": config_data['realm'],
+            "scope": config_data['scope']
+        }
+
+        header = {
+            "Content-Type": "application/json"
+        }
+        response = requests.post(LOGIN_URL, data=json.dumps(data), headers=header)
+        try:
+            encode = str(response.json()['access_token'])
+        except:
+            print "Cannot able to parse the token in Login"
+            sys.exit(1)
+        return encode, response.json()
 
     def request(self, method=None, url=None, payload=None):
         """ Do the actual request with validation """
