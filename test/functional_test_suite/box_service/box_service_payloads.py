edcb1ec12c01af70fae7b99a0ef004f3ed8d50bf
emanohar80 <emanohar80@gmail.com>
2018-02-01 18:29:13 +0530

    added payloads for box srevice

--- test/functional_test_suite/box_service/box_service_payloads.py
+++ test/functional_test_suite/box_service/box_service_payloads.py
@@ -4,9 +4,31 @@
 class BoxServicePayloads(object):
     """ payloads for box service are goes here """
 
-    def update_box_payload(self, sample='one'):
+    def action_box_payload(self,
+                           box_id='ASDFGH', agent_id='345728',
+                           job_id='9273ba2c-d4ac-41a0-b609-1ce5526051ad',
+                           order_id=None,
+                           customer_id='66214cc36fa1a900b9e847dc5d3ee474',
+                           credentials=None):
         """ payload for box update"""
         payload = {
-            "sample": sample
+            "box_id": box_id,
+            "agent_id": agent_id,
+            "job_id": job_id,
+            "order_id": order_id,
+            "customer_id": customer_id,
+            "credentials": credentials
+        }
+        return payload
+
+    def update_box_payload(self, agent_id='ASDFGH',
+                           job_id='9273ba2c-d4ac-41a0-b609-1ce5526051ad',
+                           customer_id='66214cc36fa1a900b9e847dc5d3ee474'):
+        """ Pay load to create box """
+
+        payload = {
+            "agent_id": agent_id,
+            "job_id": job_id,
+            "customer_id": customer_id
         }
         return payload
