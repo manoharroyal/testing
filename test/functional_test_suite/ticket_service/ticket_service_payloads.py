e7b6395d42a47a9d9e9b70144165a8ea93cdd7b9
emanohar80 <emanohar80@gmail.com>
2018-02-12 18:28:28 +0530

    added payload to get list of tickets

--- test/functional_test_suite/ticket_service/ticket_service_payloads.py
+++ test/functional_test_suite/ticket_service/ticket_service_payloads.py
@@ -4,10 +4,18 @@
 class TicketServicePayload(object):
     """ Class for Ticket Service Payloads """
 
-    def update_ticket_payload(self, detail="string", message="string"):
+    def update_ticket_payload(self, ticket_status="string",
+                              update_description="string"):
         """ payload to update the ticket status """
         payload = {
-            "detail": detail,
-            "message": message
+            "ticket_status": ticket_status,
+            "update_description": update_description
+        }
+        return payload
+
+    def get_tickets_payload(self, job_id="abcsdhuiddwshuah"):
+        """ payload to get list of tickets """
+        payload = {
+            "job_id": job_id
         }
         return payload
