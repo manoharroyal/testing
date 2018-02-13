8b3e200cc7ea1649c4dc5a3f5f0f3380ce3ceaed
emanohar80 <emanohar80@gmail.com>
2018-02-13 18:57:08 +0530

    added payload

--- test/functional_test_suite/ticket_service/ticket_service_payloads.py
+++ test/functional_test_suite/ticket_service/ticket_service_payloads.py
@@ -4,8 +4,8 @@
 class TicketServicePayload(object):
     """ Class for Ticket Service Payloads """
 
-    def update_ticket_payload(self, ticket_status="string",
-                              update_description="string"):
+    def update_ticket_payload(self, ticket_status="Accepted",
+                              update_description="Required one"):
         """ payload to update the ticket status """
         payload = {
             "ticket_status": ticket_status,
@@ -13,7 +13,7 @@ class TicketServicePayload(object):
         }
         return payload
 
-    def get_tickets_payload(self, job_id="abcsdhuiddwshuah"):
+    def get_tickets_payload(self, job_id="c01f5074-3f8c-42b6-a2ff-4f67de41b357"):
         """ payload to get list of tickets """
         payload = {
             "job_id": job_id
