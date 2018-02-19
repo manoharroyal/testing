7681fbb4162fe8949a907c32587ab14a73826471
emanohar80 <emanohar80@gmail.com>
2018-02-19 17:42:44 +0530

    removed payload to get the list of tickets

--- test/functional_test_suite/ticket_service/ticket_service_payloads.py
+++ test/functional_test_suite/ticket_service/ticket_service_payloads.py
@@ -12,10 +12,3 @@ class TicketServicePayload(object):
             "update_description": update_description
         }
         return payload
-
-    def get_tickets_payload(self, job_id="c01f5074-3f8c-42b6-a2ff-4f67de41b357"):
-        """ payload to get list of tickets """
-        payload = {
-            "job_id": job_id
-        }
-        return payload
