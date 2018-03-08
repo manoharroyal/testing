6d892f9ea2cef2b7d8d80deffa804523dd2c8de1
emanohar80 <emanohar80@gmail.com>
2018-03-08 12:03:36 +0530

    refactored test case code with valid box id

--- test/functional_test_suite/box_service/box_service.py
+++ test/functional_test_suite/box_service/box_service.py
@@ -3,13 +3,13 @@ import unittest
 import httplib
 from test.shared.rest_framework import RequestType, RestAPI, path
 from test.functional_test_suite.common.config import BOX_SERVICE_URL, \
-    initialize_logger, box_action_url
+    initialize_logger, box_action_url, get_box_url
 from test.functional_test_suite.box_service.box_service_payloads import BoxServicePayloads
 
 box_service = RestAPI(utype='sysops')
 box_service_invalid_token = RestAPI(utype='invalid')
 box_service_agent = RestAPI(utype='agent')
-
+job_id = "a55313f1-4810-452c-b920-db62b24dc90b"
 initialize_logger(path + "/../../logs/box_service.log")
 
 
@@ -18,11 +18,12 @@ class BoxServiceTestCases(unittest.TestCase):
 
     """ GET: test cases """
 
-    def test_get_box_details_with_valid_id(self):
+    def test_get_box_details_with_valid_job_id(self):
         """ test cases for get method """
 
         box_service_response = box_service.request(
-            RequestType.GET, BOX_SERVICE_URL)
+            RequestType.GET, get_box_url(job_id))
+        box_service_response_dict = box_service_response.json()
         logging.info('test_get_box_details_with_valid_id')
         logging.info('Url is %s', BOX_SERVICE_URL)
         logging.info('Response is %s' % box_service_response.text)
@@ -31,6 +32,9 @@ class BoxServiceTestCases(unittest.TestCase):
             msg='Expected code is 200 and got is %s (%s)' %
                 (box_service_response.status_code,
                  httplib.responses[box_service_response.status_code]))
+        self.assertIn(
+            'boxes', box_service_response_dict.keys(),
+            msg="%s in %s" % ('boxes', box_service_response_dict.keys()))
         logging.info('test case executed successfully')
 
     def test_get_box_details_with_invalid_token(self):
@@ -39,7 +43,7 @@ class BoxServiceTestCases(unittest.TestCase):
         expected_message = 'Unauthorized'
 
         box_service_response = box_service_invalid_token.request(
-            RequestType.GET, BOX_SERVICE_URL)
+            RequestType.GET, get_box_url(job_id=job_id))
         box_service_response_dict = box_service_response.json()
         logging.info('test_get_box_details_with_invalid_token')
         logging.info('Url is %s', BOX_SERVICE_URL)
@@ -47,6 +51,28 @@ class BoxServiceTestCases(unittest.TestCase):
         self.assertEquals(
             box_service_response.status_code, 401,
             msg="Expected 401 and got is %s (%s)" %
+                (box_service_response.status_code,
+                 httplib.responses[box_service_response.status_code]))
+        self.assertEquals(
+            expected_message, box_service_response_dict['message'],
+            msg="Expected message %s is equals to %s" %
+                (expected_message, box_service_response_dict['message']))
+        logging.info('test case executed successfully')
+
+    def test_get_box_details_with_invalid_job_id(self):
+        """ testing with invalid token to get box details """
+
+        expected_message = "No results found"
+
+        box_service_response = box_service.request(
+            RequestType.GET, get_box_url(job_id='1234'))
+        box_service_response_dict = box_service_response.json()
+        logging.info('test_get_box_details_with_invalid_token')
+        logging.info('Url is %s', BOX_SERVICE_URL)
+        logging.info('Response is %s' % box_service_response.text)
+        self.assertEquals(
+            box_service_response.status_code, 200,
+            msg="Expected 200 and got is %s (%s)" %
                 (box_service_response.status_code,
                  httplib.responses[box_service_response.status_code]))
         self.assertEquals(
