8cdc0fbf93562a0ff6b414f907c48521c12189e6
emanohar80 <emanohar80@gmail.com>
2018-03-22 11:10:44 +0530

    refactored token generations

--- test/shared/rest_framework.py
+++ test/shared/rest_framework.py
@@ -3,8 +3,8 @@ import os
 import sys
 import requests
 import jwt
-from enum import Enum
 import yaml
+from enum import Enum
 path = os.path.dirname(os.path.realpath(__file__))
 with open(path + "/../../env/configuration.yaml", 'r') as stream:
     try:
@@ -12,10 +12,10 @@ with open(path + "/../../env/configuration.yaml", 'r') as stream:
     except yaml.YAMLError as exc:
         print "Cannot able to access input configuration"
 
-USER_LOGIN_URL = config_data['USER_LOGIN_URL']
 LOGIN_URL = config_data['LOGIN_URL']
 
 """ Pre Function """
+
 
 def pre_check(key):
     if config_data.get(key) != None:
@@ -71,39 +71,39 @@ class RestAPI(object):
 
         data = {
             "client_id": config_data['client_id'],
+            "audience": config_data["audience"],
+            "grant_type": config_data['grant_type'],
             "username": config_data['customer_username'],
             "password": config_data['customer_password'],
-            "connection": config_data['customer_connection'],
-            "grant_type": config_data['customer_grant_type'],
-            "scope": config_data['customer_scope']
+            "realm": config_data['customer_realm'],
+            "scope": config_data['scope']
         }
 
         header = {
             "Content-Type": "application/json"
         }
-        response = requests.post(USER_LOGIN_URL, data=json.dumps(data),
+        response = requests.post(LOGIN_URL, data=json.dumps(data),
                                  headers=header)
         try:
-            encode = str(response.json()['id_token'])
+            encode = str(response.json()['access_token'])
 
         except:
             print "Cannot able to parse the token in Login"
             sys.exit(1)
         decode = jwt.decode(encode, 'secret', algorithm=['RS256'], verify=False)
-        customer_id = decode['customerId']
+        customer_id = decode['https://dev.teradatacloud.io/customerId']
         return encode, customer_id
 
     def generate_customer_invalid_token(self):
         """ Function to generate invalid token """
         encode, customer_id = self.generate_customer_token()
-        encode = encode + 'as'
+        encode = "as" + encode + 'as'
         return encode, customer_id
 
     def generate_sysops_token(self):
 
         data = {
             "client_id": config_data['client_id'],
-            "client_secret": config_data['client_secret'],
             "audience": config_data["audience"],
             "grant_type": config_data['grant_type'],
             "username": config_data['sysops_username'],
@@ -128,12 +128,11 @@ class RestAPI(object):
 
         data = {
             "client_id": config_data['client_id'],
-            "client_secret": config_data['client_secret'],
             "audience": config_data["audience"],
             "grant_type": config_data['grant_type'],
             "username": config_data['agent_username'],
             "password": config_data['agent_password'],
-            "realm": config_data['realm'],
+            "realm": config_data['agent_realm'],
             "scope": config_data['scope']
         }
 
@@ -158,20 +157,20 @@ class RestAPI(object):
         try:
 
             if method == RequestType.GET:
-                self.response = requests.get(url, headers=header, timeout=10)
-
+                self.response = requests.get(
+                    url, headers=header, timeout=30)
             elif method == RequestType.POST:
-                self.response = requests.post(url, headers=header, timeout=10,
-                                              data=json.dumps(payload))
+                self.response = requests.post(
+                    url, headers=header, timeout=30, data=json.dumps(payload))
             elif method == RequestType.PUT:
-                self.response = requests.put(url, headers=header, timeout=10,
-                                             data=json.dumps(payload))
+                self.response = requests.put(
+                    url, headers=header, timeout=30, data=json.dumps(payload))
             elif method == RequestType.DELETE:
-                self.response = requests.delete(url, headers=header, timeout=10)
-
+                self.response = requests.delete(
+                    url, headers=header, timeout=30)
             elif method == RequestType.PATCH:
-                self.response = requests.patch(url, headers=header, timeout=10,
-                                               data=json.dumps(payload))
+                self.response = requests.patch(
+                    url, headers=header, timeout=30, data=json.dumps(payload))
             return self.response
 
         except:
